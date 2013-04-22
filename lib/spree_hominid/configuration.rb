8e3cb7ec013587bfc266834be61ee1c4ce641d2b
Josh Nussbaum <joshnuss@gmail.com>
2013-04-22 16:06:52 -0400

    Adds default merge vars to configuration options

--- lib/spree_hominid/configuration.rb
+++ lib/spree_hominid/configuration.rb
@@ -1,5 +1,5 @@
 class SpreeHominid::Configuration < Spree::Configuration
-  preference :key, :string
-  preference :list_name, :string, default: 'Members'
-  preference :merge_vars, :object, default: []
+  preference :key,        :string
+  preference :list_name,  :string, default: 'Members'
+  preference :merge_vars, :hash,   default: {'EMAIL' => :email}
 end
