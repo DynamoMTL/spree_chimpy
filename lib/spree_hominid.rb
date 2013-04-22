3640fc61d6be96ef78caf9bb36489bb5bb1305fa
Josh Nussbaum <joshnuss@gmail.com>
2013-04-22 13:46:19 -0400

    Adds specs for SpreeHominid::List

--- lib/spree_hominid.rb
+++ lib/spree_hominid.rb
@@ -1,2 +1,3 @@
 require 'spree_core'
 require 'spree_hominid/engine'
+require 'hominid'
--- lib/spree_hominid/list.rb
+++ lib/spree_hominid/list.rb
@@ -2,13 +2,22 @@ module SpreeHominid
   module List
     extend self
 
+    def api
+      @api ||= Hominid::API.new(Config.preferred_key)
+    end
+
     def subscribe(user)
+      api.list_subscribe(list['id'], user.email)
     end
 
     def unsubscribe(user)
     end
 
     def sync(user)
+    end
+
+    def list
+      @list ||= api.find_by_name(Config.preferred_list_name)
     end
   end
 end
