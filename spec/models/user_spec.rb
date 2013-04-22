a5efe53c0ccbdc7bd3ca0ab2e2b4b1442c0699c6
Josh Nussbaum <joshnuss@gmail.com>
2013-04-22 15:57:16 -0400

    Fixed failing user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe Spree::User do
   context "syncing with mail chimp" do
-    let(:user)         { FactoryGirl.create(:user) }
     let(:subscription) { mock(:subscription) }
 
     before do
       subscription.should_receive(:subscribe)
-      SpreeHominid::Subscription.should_receive(:new).with(user).and_return(subscription)
+      SpreeHominid::Subscription.should_receive(:new).at_least(1).and_return(subscription)
+      @user = FactoryGirl.create(:user)
     end
 
     it "submits after saving" do
       subscription.should_receive(:sync)
 
-      user.save
+      @user.save
     end
 
     it "submits after destroy" do
       subscription.should_receive(:unsubscribe)
 
-      user.destroy
+      @user.destroy
     end
   end
 end
