29c5abf1e4b956ba7d89e8bb6120829b5b3c1eb5
Josh Nussbaum <joshnuss@gmail.com>
2013-04-22 16:52:12 -0400

    Adds check to see if mailchimp is enabled

--- lib/spree_hominid/subscription.rb
+++ lib/spree_hominid/subscription.rb
@@ -3,15 +3,18 @@ module SpreeHominid
     API_VERSION = '1.3'
 
     def initialize(user)
+      return unless enabled?
       @user = user
       @api  = Hominid::API.new(SpreeHominid::Config.preferred_key, api_version: API_VERSION)
     end
 
     def subscribe
+      return unless enabled?
       @api.list_subscribe(list['id'], @user.email)
     end
 
     def unsubscribe
+      return unless enabled?
       @api.list_unsubscribe(list['id'], @user.email)
     end
 
@@ -20,6 +23,11 @@ module SpreeHominid
 
     def list
       @list ||= @api.find_list_by_name(Config.preferred_list_name)
+    end
+
+  private
+    def enabled?
+      Config.preferred_key.present?
     end
   end
 end
--- spec/lib/spree_hominid/subscription_spec.rb
+++ spec/lib/spree_hominid/subscription_spec.rb
@@ -1,31 +1,50 @@
 require 'spec_helper'
 
 describe SpreeHominid::Subscription do
-  let(:user)         { FactoryGirl.build(:user) }
   let(:hominid)      { mock(:hominid) }
-  let(:subscription) { SpreeHominid::Subscription.new(user) }
+ 
+  context "mail chimp enabled" do
+    let(:user)         { FactoryGirl.build(:user) }
+    let(:subscription) { SpreeHominid::Subscription.new(user) }
 
-  before do
-    SpreeHominid::Config.preferred_key       = 'secret'
-    SpreeHominid::Config.preferred_list_name = 'Members'
+    before do
+      SpreeHominid::Config.preferred_key       = 'secret'
+      SpreeHominid::Config.preferred_list_name = 'Members'
 
-    Hominid::API.should_receive(:new).with('secret', api_version: '1.3').and_return(hominid)
+      Hominid::API.should_receive(:new).with('secret', api_version: '1.3').and_return(hominid)
+    end
+
+    it "subscribes" do
+      hominid.should_receive(:find_list_by_name).with("Members").and_return('id' => 1234)
+      hominid.should_receive(:list_subscribe).with(1234, user.email)
+
+      subscription.subscribe
+    end
+
+    context "sync" do
+    end
+
+    it "unsubscribes" do
+      hominid.should_receive(:find_list_by_name).with("Members").and_return('id' => 1234)
+      hominid.should_receive(:list_unsubscribe).with(1234, user.email)
+
+      subscription.unsubscribe
+    end
   end
 
-  it "subscribes" do
-    hominid.should_receive(:find_list_by_name).with("Members").and_return('id' => 1234)
-    hominid.should_receive(:list_subscribe).with(1234, user.email)
+  context "mail chimp disabled" do
+    before do
+      SpreeHominid::Config.preferred_key = nil
+      Hominid::API.should_not_receive(:new)
 
-    subscription.subscribe
+      user = FactoryGirl.build(:user)
+      @subscription = SpreeHominid::Subscription.new(user)
+
+      hominid.should_not_receive(:list_unsubscribe)
+      hominid.should_not_receive(:list_subscribe)
+    end
+
+    specify { @subscription.subscribe }
   end
 
-  context "sync" do
-  end
-
-  it "unsubscribes" do
-    hominid.should_receive(:find_list_by_name).with("Members").and_return('id' => 1234)
-    hominid.should_receive(:list_unsubscribe).with(1234, user.email)
-
-    subscription.unsubscribe
-  end
 end
