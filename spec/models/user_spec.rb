f465865ed558bb6d8e7bcbec3d04707e7324e413
Josh Nussbaum <joshnuss@gmail.com>
2013-04-22 14:07:32 -0400

    Refactors user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe Spree::User do
   context "syncing with mail chimp" do
-    it "submits after creating" do
+    before do
       SpreeHominid::List.should_receive(:subscribe)
+    end
+
+    it "submits after creating" do
       FactoryGirl.create(:user)
     end
 
     it "submits after saving" do
-      SpreeHominid::List.should_receive(:subscribe)
       SpreeHominid::List.should_receive(:sync)
       FactoryGirl.create(:user).save
     end
