2257b25d4dc2658c5d8baccfa52131cb19b9e4c4
Josh Nussbaum <joshnuss@gmail.com>
2013-04-23 13:05:09 -0400

    Adds specs for syncing user

--- spec/lib/spree_hominid/subscription_spec.rb
+++ spec/lib/spree_hominid/subscription_spec.rb
@@ -4,27 +4,63 @@ describe SpreeHominid::Subscription do
 
   context "mail chimp enabled" do
     let(:interface)    { mock(:interface) }
-    let(:user)         { FactoryGirl.build(:user) }
-    let(:subscription) { SpreeHominid::Subscription.new(user) }
 
     before do
       SpreeHominid::Config.preferred_list_name = 'Members'
       SpreeHominid::Config.stub(interface: interface)
     end
 
-    it "subscribes" do
-      interface.should_receive(:subscribe).with('Members', user.email)
+    context "subscribing" do
+      let(:user)         { FactoryGirl.build(:user) }
+      let(:subscription) { SpreeHominid::Subscription.new(user) }
 
-      subscription.subscribe
+      it "subscribes" do
+        interface.should_receive(:subscribe).with('Members', user.email)
+
+        subscription.subscribe
+      end
     end
 
     context "sync" do
+      let(:user)         { FactoryGirl.create(:user) }
+      let(:subscription) { SpreeHominid::Subscription.new(user) }
+
+      before do
+        interface.should_receive(:subscribe).with('Members', user.email)
+      end
+
+      context "user not modified" do
+        it "does nothing" do
+          interface.should_not_receive(:unsubscribe)
+          subscription.sync
+        end
+      end
+
+      context "user modified subscribed attributes" do
+        context "subscribed to unsubscribed" do
+          before { user.update_attributes(subscribed: true)}
+
+          context "unsubscribed to subscribed" do
+            before { user.update_attributes(subscribed: false)}
+          end
+        end
+      end
+
+      context "user modified non-tracked attributes"
+      context "user modified tracked attribute" do
+        context "has changed? method"
+        context "doesnt have changed? method"
+      end
     end
 
-    it "unsubscribes" do
-      interface.should_receive(:unsubscribe).with('Members', user.email)
+    context "subscribing" do
+      let(:user)         { FactoryGirl.build(:user) }
+      let(:subscription) { SpreeHominid::Subscription.new(user) }
 
-      subscription.unsubscribe
+      it "unsubscribes" do
+        interface.should_receive(:unsubscribe).with('Members', user.email)
+        subscription.unsubscribe
+      end
     end
   end
 
@@ -38,6 +74,7 @@ describe SpreeHominid::Subscription do
 
     specify { @subscription.subscribe }
     specify { @subscription.unsubscribe }
+    specify { @subscription.sync }
   end
 
 end
