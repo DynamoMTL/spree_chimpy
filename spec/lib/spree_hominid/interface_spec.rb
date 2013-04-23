94b4342f4a5e97b06ace57515f2c4e31ef47e0fa
Josh Nussbaum <joshnuss@gmail.com>
2013-04-23 11:14:05 -0400

    Adds implementation for Interface#subscribe

---
+++ spec/lib/spree_hominid/interface_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+module SpreeHominid
+  class Interface
+    def initialize(key)
+      @api       = Hominid::API.new(key)
+    end
+
+    def subscribe(list_name, email)
+      list_id = @api.find_list_id_by_name(list_name)
+      @api.list_subscribe(list_id, email)
+    end
+  end
+end
+
+describe SpreeHominid::Interface do
+  let(:interface) { SpreeHominid::Interface.new('1234') }
+  let(:api)       { mock(:api) }
+
+  before do
+    Hominid::API.should_receive(:new).with('1234').and_return(api)
+  end
+
+  it "#subscribes" do
+    api.should_receive(:find_list_id_by_name).with('Members').and_return('a3d3')
+    api.should_receive(:list_subscribe).with('a3d3', 'user@example.com')
+    interface.subscribe('Members', "user@example.com")
+  end
+
+  context "#unsubscribe" do
+  end
+
+  context "#find_list" do
+  end
+
+  # subscribe
+  # unsubscribe
+  # find_list
+  # add_merge_var
+  # find_merge_var
+end
