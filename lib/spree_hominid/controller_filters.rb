3c51b53f8678f46e1e2175671757144190bec7f3
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 18:50:21 -0400

    Updates controller filters to use correct param name

--- lib/spree_hominid/controller_filters.rb
+++ lib/spree_hominid/controller_filters.rb
@@ -2,11 +2,11 @@ module SpreeHominid
   module ControllerFilters
     extend ActiveSupport::Concern
 
-    included { before_filter :find_campaign }
+    included { before_filter :find_mail_chimp_email_id }
 
   private
-    def find_campaign
-      session[:campaign_id] = params[:mc_id] if params[:mc_id]
+    def find_mail_chimp_email_id
+      session[:mc_eid] = params[:mc_eid] if params[:mc_eid]
     end
   end
 end
