02aad11b7838c9ad0e49cba5b330fc64b0ec75cd
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 11:33:28 -0400

    Moves list name checking from initializer into config class

--- lib/spree_hominid/configuration.rb
+++ lib/spree_hominid/configuration.rb
@@ -11,5 +11,9 @@ module SpreeHominid
     def interface
       Interface.new(preferred_key, preferred_list_name) if enabled?
     end
+
+    def list_exists?
+      interface.find_list_id(preferred_list_name)
+    end
   end
 end
--- lib/spree_hominid/engine.rb
+++ lib/spree_hominid/engine.rb
@@ -19,8 +19,8 @@ module SpreeHominid
 
     initializer 'spree_hominid.check_list_name' do
       if Config.enabled?
-        list_name = Config.preferred_list_name
-        Rails.logger.error("spree_hominid: hmm.. a list named `#{list_name}` was not found. please add it and reboot the app") unless Config.interface.find_list_id(list_name)
+        list_name = SpreeHominid::Config.preferred_list_name
+        Rails.logger.error("spree_hominid: hmm.. a list named `#{list_name}` was not found. please add it and reboot the app") unless Config.list_exists?
       end
     end
 
