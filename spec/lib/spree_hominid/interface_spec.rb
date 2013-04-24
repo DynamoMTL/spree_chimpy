ae989fb71d9e900bee082f68dad696af019701e8
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 18:45:01 -0400

    Reformats spec for Interface#add_order

--- spec/lib/spree_hominid/interface_spec.rb
+++ spec/lib/spree_hominid/interface_spec.rb
@@ -41,20 +41,27 @@ describe SpreeHominid::Interface do
   it "can add an order" do
     Spree::Config.preferred_site_name = "Super Store"
     SpreeHominid::Config.preferred_store_id = "super-store"
-    variant = OpenStruct.new(id: 10, sku: 'WDG-22', name: 'Widget', cost_price: 10)
-    lines = [OpenStruct.new(variant: variant, quantity: 2)]
-    order = OpenStruct.new(id: 123, email: 'user@example.com', total: 100, completed_at: Date.new(2015, 1, 1), tax_total: 0.50, ship_total: 1.50, line_items: lines)
+
+    variant  = OpenStruct.new(id: 10, sku: 'WDG-22', name: 'Widget', cost_price: 10)
+    line     = OpenStruct.new(variant: variant, quantity: 2)
+    order    = OpenStruct.new(id: 123, email: 'user@example.com', total: 100, completed_at: Date.new(2015, 1, 1), tax_total: 0.50, ship_total: 1.50, line_items: [line])
     expected = {
-      id: 123,
-      email_id: nil,
-      email: 'user@example.com',
-      total: 100,
+      id:         123,
+      email_id:   nil,
+      email:      'user@example.com',
+      total:      100,
       order_date: Date.new(2015,1,1),
-      shipping: 1.50,
-      tax: 0.50,
+      shipping:   1.50,
+      tax:        0.50,
       store_name: 'Super Store',
-      store_id: 'super-store',
-      :items=>[{product_id: 10, sku: "WDG-22", product_name: "Widget", cost: 10, qty: 2}]
+      store_id:   'super-store',
+      items:      [{
+                     product_id:   10,
+                     sku:          "WDG-22",
+                     product_name: "Widget",
+                     cost:         10,
+                     qty:          2
+                  }]
     }
 
     api.should_receive(:ecomm_order_add).with(expected).and_return(true)
