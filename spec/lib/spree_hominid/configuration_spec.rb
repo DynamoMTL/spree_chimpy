c7b2af70ec775c13e563bae2db8bbdce626aceee
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 12:59:27 -0400

    Refactors merge var syncing specs

--- spec/lib/spree_hominid/configuration_spec.rb
+++ spec/lib/spree_hominid/configuration_spec.rb
@@ -19,29 +19,28 @@ describe SpreeHominid::Configuration do
 
   context "sync merge vars" do
     let(:interface) { mock(:interface) }
+    let(:configuration) { config(key: '1234',
+                                 list_name: 'Members',
+                                 merge_vars: {'EMAIL' => :email, 'FNAME' => :first_name, 'LNAME' => :last_name})}
     before { SpreeHominid::Interface.should_receive(:new).any_number_of_times.with('1234', 'Members').and_return(interface) }
 
     it "adds var for each" do
       interface.should_receive(:merge_vars).and_return([])
       interface.should_receive(:add_merge_var).with('FNAME', 'First Name')
       interface.should_receive(:add_merge_var).with('LNAME', 'Last Name')
-      c = config(key: '1234',
-                 list_name: 'Members',
-                 merge_vars: {'EMAIL' => :email, 'FNAME' => :first_name, 'LNAME' => :last_name})
-      c.sync_merge_vars
+
+      configuration.sync_merge_vars
     end
 
     it "skips vars that exist" do
       interface.should_receive(:merge_vars).and_return(%w(EMAIL FNAME))
       interface.should_receive(:add_merge_var).with('LNAME', 'Last Name')
-      c = config(key: '1234',
-                 list_name: 'Members',
-                 merge_vars: {'EMAIL' => :email, 'FNAME' => :first_name, 'LNAME' => :last_name})
 
-      c.sync_merge_vars
+      configuration.sync_merge_vars
     end
 
-    it "doesnt sync if all exist"
+    it "doesnt sync if all exist" do
+    end
   end
 
   def config(options = {})
