ae7219d734e8d7c38bb1369f95d6c7dbbef6d1b0
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 23:04:53 -0400

    Sets table_name explicitly in model, otherwise AR complained

--- app/models/spree_hominid/order_source.rb
+++ app/models/spree_hominid/order_source.rb
@@ -1,4 +1,5 @@
 class SpreeHominid::OrderSource < ActiveRecord::Base
+  self.table_name = :spree_hominid_order_sources
   belongs_to :order, class_name: 'Spree::Order'
 
   attr_accessible :campaign_id, :email_id
