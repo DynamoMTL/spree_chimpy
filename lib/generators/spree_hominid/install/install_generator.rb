efddda27ac841773a2bdd7225cba476952d575bd
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 23:27:37 -0400

    Removes uneeded assets

--- lib/generators/spree_hominid/install/install_generator.rb
+++ lib/generators/spree_hominid/install/install_generator.rb
@@ -1,17 +1,6 @@
 module Spree::Hominid
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_hominid\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_hominid\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_hominid\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_hominid\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_hominid'
       end
