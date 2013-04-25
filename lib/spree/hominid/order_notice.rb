8ca0f2228724b977ec5864e6be891ccc74a51e4d
Josh Nussbaum <joshnuss@gmail.com>
2013-04-25 13:50:39 -0400

    Adds OrderNotice spec

--- lib/spree/hominid/order_notice.rb
+++ lib/spree/hominid/order_notice.rb
@@ -1,6 +1,28 @@
 module Spree::Hominid
   class OrderNotice
     def initialize(order)
+      @order = order
+
+      sync
+    end
+
+    def sync
+      if Config.configured?
+        remove if exists?
+        add unless @order.canceled?
+      end
+    end
+
+    def exists?
+      Config.orders.exists?(@order.number)
+    end
+
+    def remove
+      Config.orders.remove(@order.number)
+    end
+
+    def add
+      Config.orders.add(@order)
     end
   end
 end
--- spec/lib/order_notice_spec.rb
+++ spec/lib/order_notice_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
 describe Spree::Hominid::OrderNotice do
-  
+  context "syncing order" do
+    let(:notice)    { Spree::Hominid::OrderNotice.new(order) }
+    let(:interface) { mock(:interface) }
+
+    before do
+      Spree::Hominid::Config.preferred_key = nil
+      Spree::Hominid::Config.stub(orders: interface)
+    end
+
+    context "canceled" do
+      let(:order) { FactoryGirl.create(:completed_order_with_totals, state: 'canceled')}
+
+      it "removes order" do
+        interface.should_receive(:exists?).with(order.number).and_return(true)
+        interface.should_receive(:remove).with(order.number)
+
+        Spree::Hominid::Config.preferred_key = '1234'
+        Spree::Hominid::OrderNotice.new(order)
+      end
+    end
+
+    context "completed" do
+      let(:order) { FactoryGirl.create(:completed_order_with_totals)}
+
+      context "order already exists in mail chimp" do
+        it "removes order first" do
+          interface.should_receive(:exists?).with(order.number).and_return(true)
+          interface.should_receive(:remove).with(order.number)
+          interface.should_receive(:add).with(order)
+
+          Spree::Hominid::Config.preferred_key = '1234'
+          Spree::Hominid::OrderNotice.new(order)
+        end
+      end
+
+      context "order does not exist in mail chimp" do
+        it "adds order" do
+          interface.should_receive(:exists?).with(order.number).and_return(false)
+          interface.should_receive(:add).with(order)
+          interface.should_not_receive(:remove)
+
+          Spree::Hominid::Config.preferred_key = '1234'
+          Spree::Hominid::OrderNotice.new(order)
+        end
+      end
+    end
+  end
 end
