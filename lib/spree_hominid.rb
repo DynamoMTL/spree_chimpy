69b5137cd41c20582a49b54cc5fc53d47de76f75
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 22:46:47 -0400

    Adds config helper for initializer

--- lib/spree_hominid.rb
+++ lib/spree_hominid.rb
@@ -2,3 +2,9 @@ require 'spree_core'
 require 'spree_hominid/engine'
 require 'spree_hominid/subscription'
 require 'hominid'
+
+module SpreeHominid
+  def self.config(&block)
+    yield(SpreeHominid::Config)
+  end
+end
