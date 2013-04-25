120f1c51dd6ecb999c8b3bccbb65a8649d5036d1
Josh Nussbaum <joshnuss@gmail.com>
2013-04-25 10:54:43 -0400

    Adds specs to ensure order notice is sent after order is updated

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,3 +1,10 @@
 Spree::Order.class_eval do
   has_one :source, class_name: 'Spree::Hominid::OrderSource'
+
+  around_save :notify_mail_chimp, if: :completed?
+
+private
+  def notify_mail_chimp
+    Spree::Hominid::OrderNotice.new(self)
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,4 +5,25 @@ describe Spree::Order do
     order = Spree::Order.new
     order.should respond_to(:source)
   end
+
+  context "notifying mail chimp" do
+    before do
+      Spree::Hominid::Config.preferred_key = nil
+
+      @completed_order     = FactoryGirl.build(:completed_order_with_totals)
+      @not_completed_order = FactoryGirl.build(:order)
+
+      Spree::Hominid::Config.preferred_key = '1234'
+    end
+
+    it "doesnt update when order is not completed" do
+      Spree::Hominid::OrderNotice.should_not_receive(:new)
+      @not_completed_order.save
+    end
+
+    it "updates when order is completed" do
+      Spree::Hominid::OrderNotice.should_receive(:new).with(@completed_order)
+      @completed_order.save
+    end
+  end
 end
