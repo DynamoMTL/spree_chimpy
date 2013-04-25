282e9eb30bb151d32e078e05ce8b061c68361692
Josh Nussbaum <joshnuss@gmail.com>
2013-04-24 23:05:59 -0400

    Updates mailchimp before_filters to handle creating/updating order's source

--- lib/spree_hominid/controller_filters.rb
+++ lib/spree_hominid/controller_filters.rb
@@ -2,11 +2,20 @@ module SpreeHominid
   module ControllerFilters
     extend ActiveSupport::Concern
 
-    included { before_filter :find_mail_chimp_email_id }
+    included { before_filter :find_mail_chimp_params }
 
   private
-    def find_mail_chimp_email_id
-      session[:mc_eid] = params[:mc_eid] if params[:mc_eid]
+    def find_mail_chimp_params
+      if params[:mc_eid] || params[:mc_cid]
+        attributes = {campaign_id: params[:mc_cid],
+                      email_id:    params[:mc_eid]}
+
+        if current_order(true).source
+          current_order.source.update_attributes(attributes)
+        else
+          current_order.create_source(attributes)
+        end
+      end
     end
   end
 end
