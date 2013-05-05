16aeef5edfda39a0a6a2287d286c547d680ed22d
Johan Bruning <johan.h.bruning@gmail.com>
2013-05-05 08:41:40 +0200

    Decouple migration from spree_auth_devise by using the table name based on Spree.user_class

--- db/migrate/20130422122600_add_subscribed_to_spree_users.rb
+++ db/migrate/20130422122600_add_subscribed_to_spree_users.rb
@@ -1,6 +1,6 @@
 class AddSubscribedToSpreeUsers < ActiveRecord::Migration
   def change
-    change_table :spree_users do |t|
+    change_table Spree.user_class.table_name.to_sym do |t|
       t.boolean :subscribed
     end
   end
