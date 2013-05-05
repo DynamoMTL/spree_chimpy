049b1669fdd31c6126e15294b41da9bdbabd045f
Johan Bruning <johan.h.bruning@gmail.com>
2013-05-05 08:33:32 +0200

    Decorate the user model even if it's not called Spree::User. This decouples this plugin from spree_auth_devise

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,14 +1,16 @@
-Spree::User.class_eval do
-  attr_accessible :subscribed
+if Spree.user_class
+  Spree.user_class.class_eval do
+    attr_accessible :subscribed
 
-  after_create  :subscribe
-  around_update :resubscribe
-  after_destroy :unsubscribe
+    after_create  :subscribe
+    around_update :resubscribe
+    after_destroy :unsubscribe
 
-  delegate :subscribe, :resubscribe, :unsubscribe, to: :subscription
+    delegate :subscribe, :resubscribe, :unsubscribe, to: :subscription
 
-private
-  def subscription
-    Spree::Hominid::Subscription.new(self)
+  private
+    def subscription
+      Spree::Hominid::Subscription.new(self)
+    end
   end
 end
