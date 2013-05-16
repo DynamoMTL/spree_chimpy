b0dc01aa5c9d94772b7bc204d637db4774a9defb
Michael Bianco <iloveitaly@gmail.com>
2013-05-16 10:30:22 -0400

    Coverting list wrapper to the mailchimp api gem

--- lib/spree/chimpy/interface/list.rb
+++ lib/spree/chimpy/interface/list.rb
@@ -4,17 +4,16 @@ module Spree::Chimpy
       delegate :log, to: Spree::Chimpy
 
       def initialize(key, list_name, segment_name)
-        @api          = Hominid::API.new(key, api_version: Spree::Chimpy::API_VERSION)
-        @new_api      = ::Mailchimp::API.new(key)
+        @api      = Mailchimp::API.new(key, throws_exceptions: true, timeout: 60)
         @list_name    = list_name
         @segment_name = segment_name
+        @list_name = list_name
       end
 
       def subscribe(email, merge_vars = {}, options = {})
         log "Subscribing #{email} to #{@list_name}"
 
-        @api.list_subscribe(list_id, email, merge_vars, 'html', true, true)
-        # @new_api.list_subscribe(id: list_id, email_address: email, merge_vars: merge_vars, update_existing: true)
+        @api.list_subscribe(id: list_id, email_address: email, merge_vars: merge_vars, update_existing: true, double_optin: true, email_type: 'html')
 
         segment(email) if options[:customer]
       end
@@ -22,39 +21,43 @@ module Spree::Chimpy
       def unsubscribe(email)
         log "Unsubscribing #{email} from #{@list_name}"
 
-        @api.list_unsubscribe(list_id, email)
+        @api.list_unsubscribe(id: list_id, email_address: email)
       end
 
       def merge_vars
         log "Finding merge vars for #{@list_name}"
 
-        @api.list_merge_vars(list_id).map {|record| record['tag'] }
+        @api.list_merge_vars(id: list_id).map { |record| record['tag'] }
       end
 
       def add_merge_var(tag, description)
         log "Adding merge var #{tag} to #{@list_name}"
 
-        @api.list_merge_var_add(list_id, tag, description)
+        @api.list_merge_var_add(id: list_id, tag: tag, name: description)
+      end
+
+      def find_list_id(name)
+        @api.lists["data"].detect { |r| r["name"] == name }["id"]
       end
 
       def list_id
-        @list_id ||= @api.find_list_id_by_name(@list_name)
+        @list_id ||= find_list_id(@list_name)
       end
 
       def segment(email)
         log "Adding #{email} to segment #{@segment_name}"
 
-        @api.list_static_segment_members_add(list_id, segment_id, [email])
+        @api.list_static_segment_members_add(id: list_id, seg_id: segment_id, batch: [email])
       end
 
       def create_segment
         log "Creating segment #{@segment_name}"
 
-        @segment_id = @api.list_static_segment_add(list_id, @segment_name)
+        @segment_id = @api.list_static_segment_add(id: list_id, name: @segment_name)
       end
 
       def find_segment_id
-        segments = @api.list_static_segments(list_id)
+        segments = @api.list_static_segments(id: list_id)
         segment  = segments.detect {|segment| segment['name'].downcase == @segment_name.downcase }
 
         segment['id'] if segment
