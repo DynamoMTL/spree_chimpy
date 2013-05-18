d3e0249c734ee169c43d86367b7995c822db0de3
Johan Bruning <johan.h.bruning@gmail.com>
2013-05-18 12:17:16 +0200

    Fix namespace

--- app/controllers/spree/chimpy/subscribers_controller.rb
+++ app/controllers/spree/chimpy/subscribers_controller.rb
@@ -2,10 +2,10 @@ class Spree::Chimpy::SubscribersController < ApplicationController
   respond_to :html
 
   def create
-    @subscriber = Subscriber.new(params[:subscriber])
+    @subscriber = Spree::Chimpy::Subscriber.new(params[:subscriber])
 
     if @subscriber.save
-      Subscription.new(@subscriber).subscribe
+      Spree::Chimpy::Subscription.new(@subscriber).subscribe
       flash[:notice] = I18n.t("spree.chimpy.subscriber.success")
     else
       flash[:error] = I18n.t("spree.chimpy.subscriber.failure")
