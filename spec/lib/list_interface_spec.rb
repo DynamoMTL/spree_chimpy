b5e3ca64725420ac15d5a9650006f047d64da880
Michael Bianco <iloveitaly@gmail.com>
2013-07-01 20:19:14 -0400

    Fixing list interface spec to work with mailchimp gem

--- spec/lib/list_interface_spec.rb
+++ spec/lib/list_interface_spec.rb
@@ -6,35 +6,31 @@ describe Spree::Chimpy::Interface::List do
 
   before do
     Spree::Chimpy::Config.key = '1234'
-    Hominid::API.should_receive(:new).with('1234', api_version: '1.3').and_return(api)
+    Mailchimp::API.should_receive(:new).with('1234', {:throws_exceptions=>true, :timeout=>60}).and_return(api)
+    api.should_receive(:lists).and_return({"data" => [{"name" => "Members", "id" => "a3d3" }]})
   end
 
   it "subscribes" do
-    api.should_receive(:find_list_id_by_name).with('Members').and_return('a3d3')
-    api.should_receive(:list_subscribe).with('a3d3', 'user@example.com', {'SIZE' => '10'}, 'html', true, true)
+    api.should_receive(:list_subscribe).with({:id => 'a3d3', :email_address => 'user@example.com', :merge_vars => {'SIZE' => '10'}, :email_type => 'html', :double_optin => true, :update_existing => true})
     interface.subscribe("user@example.com", 'SIZE' => '10')
   end
 
   it "unsubscribes" do
-    api.should_receive(:find_list_id_by_name).with('Members').and_return('a3d3')
-    api.should_receive(:list_unsubscribe).with('a3d3', 'user@example.com')
+    api.should_receive(:list_unsubscribe).with({:id => 'a3d3', :email_address => 'user@example.com'})
     interface.unsubscribe("user@example.com")
   end
 
   it "find list id" do
-    api.should_receive(:find_list_id_by_name).with('Members').and_return('a3d3')
     interface.list_id
   end
 
   it "checks if merge var exists" do
-    api.should_receive(:find_list_id_by_name).with('Members').and_return('a3d3')
-    api.should_receive(:list_merge_vars).with('a3d3').and_return([{'tag' => 'FOO'}, {'tag' => 'BAR'}])
+    api.should_receive(:list_merge_vars).with({:id => 'a3d3'}).and_return([{'tag' => 'FOO'}, {'tag' => 'BAR'}])
     interface.merge_vars.should == %w(FOO BAR)
   end
 
   it "adds a merge var" do
-    api.should_receive(:find_list_id_by_name).with('Members').and_return('a3d3')
-    api.should_receive(:list_merge_var_add).with('a3d3', 'SIZE', 'Your Size')
+    api.should_receive(:list_merge_var_add).with({:id => "a3d3", :tag => "SIZE", :name => "Your Size"})
     interface.add_merge_var('SIZE', 'Your Size')
   end
 end
