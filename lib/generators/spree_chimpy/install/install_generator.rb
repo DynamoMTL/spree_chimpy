737e6a49282c51ee44a7ae8204a5e65be1e1668a
Martin Tomov <mtomov@outlook.com>
2014-02-28 15:14:51 +0000

    Auto-run migrations on Circle CI

--- lib/generators/spree_chimpy/install/install_generator.rb
+++ lib/generators/spree_chimpy/install/install_generator.rb
@@ -12,12 +12,7 @@ module SpreeChimpy
       end
 
       def run_migrations
-         res = ask 'Would you like to run the migrations now? [Y/n]'
-         if res == '' || res.downcase == 'y'
-           run 'bundle exec rake db:migrate'
-         else
-           puts 'Skipping rake db:migrate, don\'t forget to run it!'
-         end
+         run 'bundle exec rake db:migrate'
       end
     end
   end
