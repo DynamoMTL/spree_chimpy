b9540513ce378f35c4992e86a29a7480873cd01d
Tobias Bohwalli <hi@futhr.io>
2014-03-27 20:02:09 +0100

    Update engine for latest Spree version.

--- Gemfile
+++ Gemfile
@@ -1,20 +1,6 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
+
+gem 'spree', github: 'spree/spree', branch: 'master'
+gem 'spree_auth_devise', github: "spree/spree_auth_devise", branch: 'master'
+
 gemspec
-
-gem 'guard'
-gem 'guard-rspec'
-gem 'rb-inotify', :require => false
-gem 'rb-fsevent', :require => false
-gem 'rb-fchange', :require => false
-gem 'libnotify'
-gem 'database_cleaner'
-gem 'fuubar'
-gem 'debugger'
-gem 'pry-rails'
-gem 'pry-debugger'
-gem 'pry-rescue'
-
-# Provides basic authentication functionality for testing parts of your engine
-gem 'spree_auth_devise', :git => "git://github.com/spree/spree_auth_devise", branch: '1-3-stable'
-gem 'spree_sample', github: 'spree/spree', branch: '1-3-stable'
-gem 'uglifier'
--- lib/generators/spree_chimpy/install/install_generator.rb
+++ lib/generators/spree_chimpy/install/install_generator.rb
@@ -1,6 +1,8 @@
 module SpreeChimpy
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, type: :boolean, default: false
+
       source_root File.expand_path('../../../templates/', __FILE__)
 
       def copy_initializer_file
@@ -12,12 +14,12 @@ module SpreeChimpy
       end
 
       def run_migrations
-         res = ask 'Would you like to run the migrations now? [Y/n]'
-         if res == '' || res.downcase == 'y'
-           run 'bundle exec rake db:migrate'
-         else
-           puts 'Skipping rake db:migrate, don\'t forget to run it!'
-         end
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
