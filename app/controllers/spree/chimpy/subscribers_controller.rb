5ed51448b2d7ccaa66bfa9a00b820bbc2e84323c
Tobias Bohwalli <hi@futhr.io>
2014-03-27 20:07:45 +0100

    Strong parameters fix by @kenjinino

--- app/controllers/spree/chimpy/subscribers_controller.rb
+++ app/controllers/spree/chimpy/subscribers_controller.rb
@@ -2,15 +2,21 @@ class Spree::Chimpy::SubscribersController < ApplicationController
   respond_to :html
 
   def create
-    @subscriber = Spree::Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first_or_initialize
-    @subscriber.update_attributes(params[:chimpy_subscriber])
+    @subscriber = Spree::Chimpy::Subscriber.where(email: subscriber_params[:email]).first_or_initialize
+    @subscriber.update_attributes(subscriber_params)
     if @subscriber.save
       Spree::Chimpy::Subscription.new(@subscriber).subscribe
-      flash[:notice] = I18n.t("spree.chimpy.subscriber.success")
+      flash[:notice] = Spree.t(:success, scope: [:chimpy, :subscriber])
     else
-      flash[:error] = I18n.t("spree.chimpy.subscriber.failure")
+      flash[:error] = Spree.t(:failure, scope: [:chimpy, :subscriber])
     end
 
     respond_with @subscriber, location: request.referer
   end
+
+  private
+
+    def subscriber_params
+      params.require(:chimpy_subscriber).permit(:email, :subscribed)
+    end
 end
