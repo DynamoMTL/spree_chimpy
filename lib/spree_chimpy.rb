3b565ab7ee472c9d5e9129a9df76a1a56d19fe01
Martin Tomov <mtomov@outlook.com>
2014-04-04 11:15:24 +0100

    Adding an order only instead of first removing it and then adding it

--- lib/spree_chimpy.rb
+++ lib/spree_chimpy.rb
@@ -112,7 +112,7 @@ module Spree::Chimpy
 
     case event
     when :order
-      orders.sync(object)
+      orders.add(object)
     when :subscribe
       list.subscribe(object.email, merge_vars(object), customer: (object.is_a?(Spree.user_class) && object.enrolled) )
     when :unsubscribe
