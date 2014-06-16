faf114f3a11f56ce1c7190c7db90e34d9e388679
Braden <braden.douglass@gmail.com>
2014-06-16 15:46:55 -0400

    Complete Move To MailChimp API V2.x For Lists
    Lists interface now uses the new MailChimp api. Along with the move to
    version 2.x, make sure all of the list specs run green.

--- lib/spree/chimpy/interface/list.rb
+++ lib/spree/chimpy/interface/list.rb
@@ -18,7 +18,7 @@ module Spree::Chimpy
       def subscribe(email, merge_vars = {}, options = {})
         log "Subscribing #{email} to #{@list_name}"
 
-        @api.lists.subscribe(id: list_id, email_address: email, merge_vars: merge_vars, update_existing: true, double_optin: @double_opt_in, email_type: 'html')
+        api_call.subscribe(id: list_id, email_address: email, merge_vars: merge_vars, update_existing: true, double_optin: @double_opt_in, email_type: 'html')
 
         segment([email]) if options[:customer]
       end
@@ -26,13 +26,14 @@ module Spree::Chimpy
       def unsubscribe(email)
         log "Unsubscribing #{email} from #{@list_name}"
 
-        @api.list_unsubscribe(id: list_id, email_address: email)
+        api_call.unsubscribe(id: list_id, email_address: email)
       end
 
       def info(email_or_id)
         log "Checking member info for #{email_or_id} from #{@list_name}"
 
-        response = @api.list_member_info(id: list_id, email_address: email_or_id)
+        #maximum of 50 emails allowed to be passed in
+        response = api_call.member_info(id: list_id, email_address: email_or_id)
         record = response['data'].first.symbolize_keys
 
         record.key?(:error) ? {} : record
@@ -41,13 +42,13 @@ module Spree::Chimpy
       def merge_vars
         log "Finding merge vars for #{@list_name}"
 
-        @api.list_merge_vars(id: list_id).map { |record| record['tag'] }
+        api_call.merge_vars(id: list_id).map { |record| record['tag'] }
       end
 
       def add_merge_var(tag, description)
         log "Adding merge var #{tag} to #{@list_name}"
 
-        @api.list_merge_var_add(id: list_id, tag: tag, name: description)
+        api_call.merge_var_add(id: list_id, tag: tag, name: description)
       end
 
       def find_list_id(name)
@@ -61,17 +62,17 @@ module Spree::Chimpy
       def segment(emails = [])
         log "Adding #{emails} to segment #{@segment_name}"
 
-        @api.list_static_segment_members_add(id: list_id, seg_id: segment_id, batch: emails)
+        api_call.static_segment_members_add(id: list_id, seg_id: segment_id, batch: emails)
       end
 
       def create_segment
         log "Creating segment #{@segment_name}"
 
-        @segment_id = @api.list_static_segment_add(id: list_id, name: @segment_name)
+        @segment_id = api_call.static_segment_add(id: list_id, name: @segment_name)
       end
 
       def find_segment_id
-        segments = @api.list_static_segments(id: list_id)
+        segments = api_call.static_segments(id: list_id)
         segment  = segments.detect {|segment| segment['name'].downcase == @segment_name.downcase }
 
         segment['id'] if segment
