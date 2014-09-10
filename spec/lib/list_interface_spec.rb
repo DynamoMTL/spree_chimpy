50da13af56679ac81e64c7911690c735b782a214
Hugo Frappier <hugo@cekoya.com>
2014-09-10 10:30:41 -0400

    Make spec use pry-byebug

--- spec/lib/list_interface_spec.rb
+++ spec/lib/list_interface_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'pry-debugger'
+require 'pry-byebug'
 
 describe Spree::Chimpy::Interface::List do
   let(:interface) { described_class.new('Members', 'customers', true, nil) }
