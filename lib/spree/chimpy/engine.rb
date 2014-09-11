45882d2289642d2d42b0bcdaaa969e453bcd83bc
Hugo Frappier <hugo@cekoya.com>
2014-09-11 14:50:53 -0400

    Temporarily disable checks at boot

--- lib/spree/chimpy/engine.rb
+++ lib/spree/chimpy/engine.rb
@@ -10,12 +10,12 @@ module Spree::Chimpy
       Spree::Chimpy::Config = Spree::Chimpy::Configuration.new
     end
 
-    initializer 'spree_chimpy.ensure' do
-      if !Rails.env.test? && Spree::Chimpy.configured?
-        Spree::Chimpy.ensure_list
-        Spree::Chimpy.ensure_segment
-      end
-    end
+    # initializer 'spree_chimpy.ensure' do
+    #   if !Rails.env.test? && Spree::Chimpy.configured?
+    #     Spree::Chimpy.ensure_list
+    #     Spree::Chimpy.ensure_segment
+    #   end
+    # end
 
     initializer 'spree_chimpy.double_opt_in' do
       if Spree::Chimpy::Config.subscribed_by_default && !Spree::Chimpy::Config.double_opt_in
@@ -40,7 +40,7 @@ module Spree::Chimpy
       if defined?(Spree::Api::BaseController)
         Spree::Api::BaseController.send(:include,  Spree::Chimpy::ControllerFilters)
       end
-      
+
       Dir.glob(File.join(File.dirname(__FILE__), '../../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
