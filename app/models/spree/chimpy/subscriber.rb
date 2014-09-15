537b4a52b45633e24f5c3fc352214d5b8a23cc38
Hugo Frappier <hugo@cekoya.com>
2014-09-15 10:42:48 -0400

    Add email format validation

--- app/models/spree/chimpy/subscriber.rb
+++ app/models/spree/chimpy/subscriber.rb
@@ -1,6 +1,10 @@
 class Spree::Chimpy::Subscriber < ActiveRecord::Base
   self.table_name = "spree_chimpy_subscribers"
+
+  EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
   validates :email, presence: true
+  validates_format_of :email, with: EMAIL_REGEX, allow_blank: false, if: :email_changed?
 
   after_create  :subscribe
   around_update :resubscribe
