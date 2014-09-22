44087305366445a2fc563cb9c69f9b41771e02f4
Hugo Frappier <hugo@cekoya.com>
2014-09-22 11:37:39 -0400

    Save order source also if record_ms_details params is present
    This way, we do not need to set session[:order_id]

--- lib/spree/chimpy/controller_filters.rb
+++ lib/spree/chimpy/controller_filters.rb
@@ -11,7 +11,7 @@ module Spree::Chimpy
       def find_mail_chimp_params
         mc_eid = params[:mc_eid] || session[:mc_eid]
         mc_cid = params[:mc_cid] || session[:mc_cid]
-        if (mc_eid || mc_cid) && session[:order_id]
+        if (mc_eid || mc_cid) && (session[:order_id] || params[:record_mc_details])
           attributes = {campaign_id: mc_cid,
                         email_id:    mc_eid}
           if current_order(create_order_if_necessary: true).source
