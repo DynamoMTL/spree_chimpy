8fe949306d6aa79b0c550a452e5e37c602fc2742
braidn <braden.douglass@gmail.com>
2015-02-04 10:47:53 -0500

    Upgrade Subscriber Spec
    Move to expect and avoid deprecations by no longer using errors_on
    
    Conflicts:
    	spec/models/subscriber_spec.rb

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe Spree::Chimpy::Subscriber do
-
-  context 'without email' do
-    it 'is invalid' do
-      expect(described_class.create).not_to be_valid
+  context "without email" do
+    it "is invalid" do
+      expect(create).to_not be_valid
     end
 
-    it 'is not valid without an email' do
-      expect(described_class.create(email: nil)).to have(1).errors_on(:email)
+    it "is not valid without an email" do
+      record = create(email: nil)
+      expect(record.errors[:email].count).to eq 1
     end
 
-    it 'can be valid' do
-      expect(described_class.create(email: 'test@example.com')).to be_valid
+    it "can be valid" do
+      expect(create(email: 'test@example.com')).to be_valid
     end
   end
 end
