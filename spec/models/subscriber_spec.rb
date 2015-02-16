503b2a4cd382286dc9eb6a6fffe226a1093efd07
braidn <braden.douglass@gmail.com>
2015-02-16 11:38:07 -0500

    Clean Up Subscriber Spec
    Failing test around having a subscriber with no email.

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -2,15 +2,6 @@ require 'spec_helper'
 
 describe Spree::Chimpy::Subscriber do
   context "without email" do
-    it "is invalid" do
-      expect(create).to_not be_valid
-    end
-
-    it "is not valid without an email" do
-      record = create(email: nil)
-      expect(record.errors[:email].count).to eq 1
-    end
-
     it 'is not valid without an email' do
       expect(described_class.new(email: nil)).to have(1).errors_on(:email)
     end
