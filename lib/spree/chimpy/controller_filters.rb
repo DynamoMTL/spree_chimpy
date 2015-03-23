5c3d59034fd7acef1dc46e5b3381146bf6dc510a
braidn <braden.douglass@gmail.com>
2015-03-23 11:33:57 -0400

    Refactor Mail Chimpy Controller Filters
    Refactor the one method private method in the mail chimpy controller.
    Add some tests to make sure that the mailchimp params are called only if
    there are params/sessions for MailChimp available

--- lib/spree/chimpy/controller_filters.rb
+++ lib/spree/chimpy/controller_filters.rb
@@ -3,23 +3,31 @@ module Spree::Chimpy
     extend ActiveSupport::Concern
 
     included do
-      before_filter :find_mail_chimp_params
+      before_filter :set_mailchimp_params
+      before_filter { |c| c.send(:find_mail_chimp_params) if c.send(:mailchimp_params?) }
       include ::Spree::Core::ControllerHelpers::Order
     end
 
 
     private
+    attr_reader :mc_eid, :mc_cid
+
+    def set_mailchimp_params
+      @mc_eid = params[:mc_eid] || session[:mc_eid]
+      @mc_cid = params[:mc_cid] || session[:mc_cid]
+    end
+
+    def mailchimp_params?
+      (!mc_eid.nil? || !mc_cid.nil?) &&
+        (!session[:order_id].nil? || !params[:record_mc_details].nil?)
+    end
+
     def find_mail_chimp_params
-      mc_eid = params[:mc_eid] || session[:mc_eid]
-      mc_cid = params[:mc_cid] || session[:mc_cid]
-      if (mc_eid || mc_cid) && (session[:order_id] || params[:record_mc_details])
-        attributes = {campaign_id: mc_cid,
-                      email_id:    mc_eid}
-        if current_order(create_order_if_necessary: true).source
-          current_order.source.update_attributes(attributes)
-        else
-          current_order.create_source(attributes)
-        end
+      attributes = {campaign_id: mc_cid, email_id: mc_eid}
+      if current_order(create_order_if_necessary: true).source
+        current_order.source.update_attributes(attributes)
+      else
+        current_order.create_source(attributes)
       end
     end
   end
--- spec/controllers/controller_filters_spec.rb
+++ spec/controllers/controller_filters_spec.rb
@@ -1,16 +1,44 @@
 require 'spec_helper'
 
 describe ::Spree::StoreController do
-
-  before do
-    subject.session[:mc_eid] = '1234'
-    subject.session[:mc_cid] = 'abcd'
+  controller(::Spree::StoreController) do
+    def index
+      head :ok
+    end
   end
 
-  it 'sets the source attributes on order' do
-    binding.pry
-    get :index
+  let(:user) { create(:user) }
+  subject { controller }
+
+  before do
+    allow(subject).to receive(:try_spree_current_user).and_return(user)
+    subject.session[:order_id] = 'R1919'
+  end
+
+  it 'sets the attributes for order if eid/cid is set in session' do
+    subject.session[:mc_eid] = '1234'
+    subject.session[:mc_cid] = 'abcd'
     expect(subject).to receive(:find_mail_chimp_params)
+
+    get :index
+  end
+
+  it 'sets the attributes for the order if eid/cid is set in the params' do
+    expect(subject).to receive(:find_mail_chimp_params)
+
+    get :index, mc_eid: '1234', mc_cid: 'abcd'
+  end
+
+  it 'does not call find mail chimp params method if no eid/cid' do
+    expect(subject).to_not receive(:find_mail_chimp_params)
+
+    get :index
+  end
+
+  it 'skips the set_current_order filter if not needed' do
+    expect(subject).to_not receive(:set_curent_order)
+
+    get :index
   end
 
 end
