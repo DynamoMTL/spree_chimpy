88a59d13bdac0794f51966aa8cb679182618766c
braidn <braden.douglass@gmail.com>
2015-03-23 13:37:19 -0400

    Order Cleanup And Adjustable Declaration
    For some reason the CI is having a problem with
    Spree::Adjustment::Adjustable. This is likely an issue with how the
    testapp is created. Just add the adjustable to this test.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
-describe Spree::Order do
+class Spree::Adjustment::Adjustable; end
 
+describe Spree::Order do
   let(:key) { 'e025fd58df5b66ebd5a709d3fcf6e600-us8' }
   let(:order) { create(:completed_order_with_totals) }
 
-  it "has a source" do
+  it 'has a source' do
     order = Spree::Order.new
     expect(order).to respond_to(:source)
   end
 
-  context "notifying mail chimp" do
+  context 'notifying mail chimp' do
     before do
       Spree::Chimpy::Config.key = nil
 
@@ -21,18 +22,18 @@ describe Spree::Order do
 
     subject { Spree::Chimpy }
 
-    it "doesnt update when order is not completed" do
+    it 'doesnt update when order is not completed' do
       expect(subject).to_not receive(:enqueue)
       @not_completed_order.update!
     end
 
-    it "updates when order is completed" do
+    it 'updates when order is completed' do
       new_order = create(:completed_order_with_pending_payment, state: 'confirm')
       expect(subject).to receive(:enqueue).with(:order, new_order)
       new_order.next
     end
 
-    it "sync when order is completed" do
+    it 'sync when order is completed' do
       expect(subject).to receive(:enqueue).with(:order, order)
       order.cancel!
     end
