7bebca725b24da1b3aa3a1a5f055b32b84f24aa3
Layon <layon1993@gmail.com>
2015-09-01 11:22:39 +0100

    create the list on the perform to avoid timeouts

--- app/models/spree/chimpy/list_subscriber.rb
+++ app/models/spree/chimpy/list_subscriber.rb
@@ -1,12 +1,14 @@
 
 class Spree::Chimpy::ListSubscriber
   def initialize(list_name, double_opt_in, email, source)
-    @list = Spree::Chimpy::Interface::List.new(list_name, 'customers', double_opt_in)
+    @list_name = list_name
+    @double_opt_in = double_opt_in
     @email = email
     @source = source
   end
 
   def perform
-    @list.direct_subscribe(@email, {"SOURCE" => @source})
+    list = Spree::Chimpy::Interface::List.new(@list_name, 'customers', @double_opt_in)
+    list.direct_subscribe(@email, {"SOURCE" => @source})
   end
 end
