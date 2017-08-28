cadc08493c9fb2931f4e61c0153ec409c5eaa708
Sanjay Prajapati <sanjay.prajapati@botreetechnologies.com>
2017-08-28 15:10:30 +0530

    Disable admin and user side deface for subscription checkbox

--- app/overrides/user_form.rb
+++ app/overrides/user_form.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_user_form",
                      :name         => "user_form_subscription",
                      :insert_after => "[data-hook=signup_below_password_fields]",
-                     :partial      => "spree/shared/user_subscription")
+                     :partial      => "spree/shared/user_subscription",
+                     :disabled => true)
